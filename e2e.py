455b686badf2fc01c8784144e42f80fb477c3565
Bian Shiyao <biansy@artc.a-star.edu.sg>
2024-12-16 17:47:09 +0800

    Enhanced E2E model

--- e2e.py
+++ e2e.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import datetime
 import matplotlib.pyplot as plt
 from invutils import get_demand, get_lead_time, get_safety_stock, get_reorder_point, get_target_inventory
-from fclib.models.dilated_cnn import create_dcnn_model
+from model import create_e2e_model
 from tensorflow.keras import optimizers
 from tensorflow.keras.models import load_model
 from tensorflow.keras.callbacks import ModelCheckpoint
@@ -21,9 +21,20 @@ MAX_STORE_ID = 50
 BATCH_SIZE = 1024
 LEARNING_RATE = 0.015
 EPOCHS = 50
+
+horizon = 120  # number of days in review period (consider x days in the future for optimal inventory position)
+train_horizon = 60
+test_horizon = horizon - train_horizon
+interval = 10  # number of days between consecutive reorder points
+forecast_horizon = 5  # number of days in advance to forecast (at dt, forecast demand from dt + 1 to dt + 5)
 seq_len = 5
 b = 9
 h = 1
+week = [i % 7 for i in range(train_horizon)]
+dynamic_features = ['inventory', 'demand', 'lead_time', 'reorder_point', 'day']
+n_dynamic_features = len(dynamic_features)
+target = ["reorder_qty", "demand", "lead_time"]
+
 model_file_name = "e2e.keras"
 log_dir = os.path.join("logs", "scalars")
 train_input_dynamic_ = []
@@ -38,27 +49,25 @@ for sku in range(1, MAX_SKU_ID + 1):
     service_level = 0.99
     for store in range(1, MAX_STORE_ID + 1):
 
-        initial_inventory = np.random.randint(50, 150)  # sku initial stock level
-        horizon = 60  # number of days in review period (consider x days in the future for optimal inventory position)
-        interval = 10  # number of days between consecutive reorder points
+        initial_inventory = np.random.randint(1, 150)  # sku initial stock level
 
-        reorder_point_arr = get_reorder_point(horizon, interval)
+        reorder_point_arr = get_reorder_point(train_horizon, interval)
         n_reorder_pts = len(np.where(reorder_point_arr)[0])  # number of reorder points in review period
         history_lead_time = get_lead_time(mu_lead_time, std_lead_time, 50)
         history_demand = abs(get_demand(mu_demand, std_demand, 50))
         future_lead_time = abs(get_lead_time(mu_lead_time, std_lead_time, n_reorder_pts))
-        future_demand = abs(get_demand(mu_demand, std_demand, horizon))
+        future_demand = abs(get_demand(mu_demand, std_demand, train_horizon))
         safety_stock = get_safety_stock(history_lead_time, history_demand, service_level)
         # e2e recover optimal re-order quantity
         t = 0
-        inventory_level = np.zeros(horizon)
+        inventory_level = np.zeros(train_horizon)
         inventory_level[0] = initial_inventory
         # generate po reaching
         po_reaching = list(np.where(reorder_point_arr)[0] + np.int32(np.ceil(future_lead_time)))
-        if po_reaching[-1] < horizon:
+        if po_reaching[-1] < train_horizon:
             po_reaching.append(po_reaching[-1] + int(np.ceil(np.diff(po_reaching).mean())))
-            optimal_rq = np.zeros(horizon)
-            for i in range(1, horizon):
+            optimal_rq = np.zeros(train_horizon)
+            for i in range(1, train_horizon):
                 inventory_level[i] = inventory_level[i - 1] + inventory_level[i] - future_demand[i]
                 if reorder_point_arr[i] and t <= len(po_reaching):
                     vm = po_reaching[t]
@@ -70,31 +79,34 @@ for sku in range(1, MAX_SKU_ID + 1):
                     t += 1
 
             # inventory level, future_demand, future_lead_time, reorder_point, reorder_
-            lead_time = np.zeros(horizon)
+            lead_time = np.zeros(train_horizon)
             lead_time[np.where(reorder_point_arr)[0]] = np.int32(np.ceil(future_lead_time))
+            # shift reorder point backward by 1 day
+            tmp = np.zeros(train_horizon)
+            rp = np.where(reorder_point_arr)[0] - 1
+            tmp[rp] = 1
+            reorder_point_arr = tmp
             data = pd.DataFrame.from_dict({'inventory': inventory_level,
                                            'demand': future_demand,
                                            'lead_time': lead_time,
                                            'reorder_point': reorder_point_arr,
-                                           'reorder_qty': optimal_rq})
+                                           'reorder_qty': optimal_rq,
+                                           'week': week})
             data["sku"] = sku
             data["store"] = store
-            data["day"] = list(range(1, horizon + 1))
+            data["day"] = list(range(1, train_horizon + 1))
 
-            dynamic_features = ['inventory', 'demand', 'lead_time', 'reorder_point', 'day']
-            n_dynamic_features = len(dynamic_features)
-            target = ["reorder_qty"]
             cate_feature = data[['sku', 'store']].values
             arr = data[dynamic_features].values
             output_arr = np.squeeze(data[target].values)
-            n = horizon - seq_len + 1
+            n = train_horizon - seq_len - forecast_horizon + 1
             train_input_dynamic = np.empty((n, seq_len, n_dynamic_features))
             for i in range(n):
                 train_input_dynamic[i, :, :] = arr[i:i + seq_len]
 
-            train_output = np.empty((n, seq_len))
-            for i in range(n):
-                train_output[i, :] = output_arr[i:i + seq_len]
+            train_output = np.zeros((n, forecast_horizon, len(target)))
+            for i in range(len(train_output)):
+                train_output[i, :] = output_arr[i + seq_len: i + seq_len + forecast_horizon, :]
             train_input_dynamic_.append(train_input_dynamic)
             train_output_.append(train_output)
             cate_feature_.append(cate_feature[:n, :])
@@ -102,11 +114,19 @@ for sku in range(1, MAX_SKU_ID + 1):
 train_output_final = np.concatenate(train_output_)
 train_input_dynamic_final = np.concatenate(train_input_dynamic_)
 cate_feature_final = np.concatenate(cate_feature_)
+
+vlt_input = train_input_dynamic_final[:, :, 2]
+rp_in = train_input_dynamic_final[:, :, 3]
+initial_stock_in = train_input_dynamic_final[:, :, 0]
+
+rp_out = train_output_final[:, :, 0][:, 0]  # replenishment decision (use history seq_len points to predict 1)
+df_out = train_output_final[:, :, 1]  # demand forecast (use history seq_len points to predict future forecast_horizon)
+vlt_out = train_output_final[:, :, 2][:, 0]  # vendor lead time forecast (use history seq_len points to predict 1)
 print(f"Running time for data generation is {time.perf_counter() - start}")
-model = create_dcnn_model(
+model = create_e2e_model(
     seq_len=seq_len,
-    n_dyn_fea=len(dynamic_features),
-    n_outputs=seq_len,
+    n_dyn_fea=len(dynamic_features) - 3,
+    n_outputs=forecast_horizon,
     n_dilated_layers=3,
     kernel_size=2,
     n_filters=3,
@@ -114,36 +134,23 @@ model = create_dcnn_model(
     max_cat_id=[MAX_SKU_ID, MAX_STORE_ID],
 )
 adam = optimizers.Adam(learning_rate=LEARNING_RATE)
-model.compile(loss="mse", optimizer=adam, metrics=["mse", "mae"])
+model.compile(loss="mse", optimizer=optimizers.Adam())
 # Define checkpoint and fit model
 checkpoint = ModelCheckpoint(model_file_name, monitor="loss", save_best_only=True, mode="min", verbose=1)
 tensorboard_callback = tf.keras.callbacks.TensorBoard(
     log_dir=os.path.join(log_dir, datetime.datetime.now().strftime("%Y%m%d-%H%M%S")), histogram_freq=1,
-)
+    write_graph=True)
 callbacks_list = [checkpoint, tensorboard_callback]
 history = model.fit(
-    [train_input_dynamic_final, cate_feature_final],
-    train_output_final,
+    [train_input_dynamic_final[:, :, (1, 4)], cate_feature_final, vlt_input, rp_in, initial_stock_in],
+    [df_out, rp_out, vlt_out],
     epochs=EPOCHS,
     batch_size=BATCH_SIZE,
     callbacks=callbacks_list,
     verbose=1,
 )
-
-for r in range(0):
-    model = load_model(model_file_name)
-    checkpoint = ModelCheckpoint(model_file_name, monitor="loss", save_best_only=True, mode="min", verbose=1)
-    callbacks_list = [checkpoint]
-    history = model.fit(
-        [train_input_dynamic_final, cate_feature_final],
-        train_output_final, epochs=1, batch_size=BATCH_SIZE, callbacks=callbacks_list, verbose=1,
-    )
-
-###############################################################
-initial_inventory = np.random.randint(50, 150)  # sku initial stock level
-horizon = 60  # number of days in review period (consider x days in the future for optimal inventory position)
-interval = 10  # number of days between consecutive reorder points
-
+# .reshape((len(rp_out), 1)).reshape((len(vlt_out), 1))
+# generate testing data
 reorder_point_arr = get_reorder_point(horizon, interval)
 n_reorder_pts = len(np.where(reorder_point_arr)[0])  # number of reorder points in review period
 history_lead_time = get_lead_time(mu_lead_time, std_lead_time, 50)
@@ -151,76 +158,87 @@ history_demand = abs(get_demand(mu_demand, std_demand, 50))
 future_lead_time = abs(get_lead_time(mu_lead_time, std_lead_time, n_reorder_pts))
 future_demand = abs(get_demand(mu_demand, std_demand, horizon))
 safety_stock = get_safety_stock(history_lead_time, history_demand, service_level)
-# e2e recover optimal re-order quantity
+# e2e policy
 t = 0
-inventory_level = np.zeros(horizon)
-inventory_level[0] = initial_inventory
+inventory_level = np.concatenate((inventory_level, np.zeros(test_horizon)))
+
+lead_time = np.zeros(horizon)
+lead_time[np.where(reorder_point_arr)[0]] = np.int32(np.ceil(future_lead_time))
+optimal_rq = np.concatenate((optimal_rq, np.zeros(test_horizon)))
 # generate po reaching
+rp = np.where(reorder_point_arr)[0] - 1
 po_reaching = list(np.where(reorder_point_arr)[0] + np.int32(np.ceil(future_lead_time)))
-if po_reaching[-1] < horizon:
-    po_reaching.append(po_reaching[-1] + int(np.ceil(np.diff(po_reaching).mean())))
-    optimal_rq = np.zeros(horizon)
-    for i in range(1, horizon):
-        inventory_level[i] = inventory_level[i - 1] + inventory_level[i] - future_demand[i]
-        if reorder_point_arr[i] and t <= len(po_reaching):
-            vm = po_reaching[t]
-            delta_t = int(np.floor(b * (po_reaching[t + 1] - po_reaching[t]) / (h + b)))  # s*
-            cum_demand = np.sum(future_demand[vm:vm + delta_t])
-            optimal_qty = max(cum_demand + np.sum(future_demand[i:vm]) - inventory_level[i], 0)
-            inventory_level[vm] += optimal_qty
-            optimal_rq[i] = optimal_qty
-            t += 1
+for i in range(train_horizon, horizon):
+    reorder_point_arr = get_reorder_point(horizon, interval)
 
-    # inventory level, future_demand, future_lead_time, reorder_point, reorder_
-    lead_time = np.zeros(horizon)
-    lead_time[np.where(reorder_point_arr)[0]] = np.int32(np.ceil(future_lead_time))
-    data = pd.DataFrame.from_dict({'inventory': inventory_level,
-                                   'demand': future_demand,
-                                   'lead_time': lead_time,
-                                   'reorder_point': reorder_point_arr,
-                                   'reorder_qty': optimal_rq})
-    data["sku"] = sku
-    data["store"] = store
-    data["day"] = list(range(1, horizon + 1))
+    inventory_level[i] = inventory_level[i - 1] + inventory_level[i] - future_demand[i]
+    if reorder_point_arr[i] or 1:
+        # calculate reorder quantity
+        tmp = np.zeros(horizon)
+        rp = np.where(reorder_point_arr)[0] - 1
+        tmp[rp] = 1
+        reorder_point_arr = tmp
+        data = pd.DataFrame.from_dict({'inventory': inventory_level[:i],
+                                       'demand': future_demand[:i],
+                                       'lead_time': lead_time[:i],
+                                       'reorder_point': reorder_point_arr[:i],
+                                       'reorder_qty': optimal_rq[:i]})
+        data["sku"] = sku
+        data["store"] = store
+        data["day"] = list(range(1, i + 1))
 
-    dynamic_features = ['inventory', 'demand', 'lead_time', 'reorder_point', 'day']
-    n_dynamic_features = len(dynamic_features)
-    target = ["reorder_qty"]
-    cate_feature = data[['sku', 'store']].values
-    arr = data[dynamic_features].values
-    output_arr = np.squeeze(data[target].values)
-    n = horizon - seq_len + 1
-    train_input_dynamic = np.empty((n, seq_len, n_dynamic_features))
-    for i in range(n):
-        train_input_dynamic[i, :, :] = arr[i:i + seq_len]
+        cate_feature = data[['sku', 'store']].values
+        arr = data[dynamic_features].values
+        output_arr = np.squeeze(data[target].values)
+        n = i - seq_len - forecast_horizon + 1
 
-    train_output = np.empty((n, seq_len))
-    for i in range(n):
-        train_output[i, :] = output_arr[i:i + seq_len]
+        train_input_dynamic = np.empty((n, seq_len, n_dynamic_features))
+        for j in range(n):
+            train_input_dynamic[j, :, :] = arr[j:j + seq_len]
 
-test = train_input_dynamic
-# test[:, :, 0] += 20
-pred = np.round(model.predict([test, cate_feature_final[0:n]]))
+        train_input_dynamic_.append(train_input_dynamic)
+        cate_feature_.append(cate_feature[:n, :])
 
-# recover inventory level
-rq = pred[:, 0][np.where(reorder_point_arr)[0]]
-t = 0
-inventory_level = np.zeros(horizon)
-inventory_level[0] = initial_inventory
-# generate po reaching
-po_reaching = list(np.where(reorder_point_arr)[0] + np.int32(np.ceil(future_lead_time)))
-if po_reaching[-1] < horizon:
-    po_reaching.append(po_reaching[-1] + int(np.ceil(np.diff(po_reaching).mean())))
-    optimal_rq = np.zeros(horizon)
-    for i in range(1, horizon):
-        inventory_level[i] = inventory_level[i - 1] + inventory_level[i] - future_demand[i]
-        if reorder_point_arr[i] and t <= len(po_reaching):
-            vm = po_reaching[t]
+        train_input_dynamic_final = np.concatenate(train_input_dynamic_)
+        cate_feature_final = np.concatenate(cate_feature_)
+        vlt_input = train_input_dynamic_final[:, :, 2]
+        rp_in = train_input_dynamic_final[:, :, 3]
+        initial_stock_in = train_input_dynamic_final[:, :, 0]
 
-            optimal_qty = rq[t]
-            inventory_level[vm] += optimal_qty
-            optimal_rq[i] = optimal_qty
-            t += 1
+        pred_len = 10
 
+        df_pred, optimal_qty, vlt_pred = model.predict([train_input_dynamic_final[-pred_len:, :, (1, 4)],
+                                                          cate_feature_final[-pred_len:], vlt_input[-pred_len:],
+                                                          rp_in[-pred_len:], initial_stock_in[-pred_len:]])
+        vm = po_reaching[t]
+        inventory_level[vm] += optimal_qty[4]
+        optimal_rq[i] = optimal_qty[4]
+        t += 1
+        print(optimal_qty)
+#
+# test = train_input_dynamic
+# # test[:, :, 0] += 20
+# pred = np.round(model.predict([test, cate_feature_final[0:n]]))
+#
+# # recover inventory level
+# rq = pred[:, 0][np.where(reorder_point_arr)[0]]
+# t = 0
+# inventory_level = np.zeros(horizon)
+# inventory_level[0] = initial_inventory
+# # generate po reaching
+# po_reaching = list(np.where(reorder_point_arr)[0] + np.int32(np.ceil(future_lead_time)))
+# if po_reaching[-1] < horizon:
+#     po_reaching.append(po_reaching[-1] + int(np.ceil(np.diff(po_reaching).mean())))
+#     optimal_rq = np.zeros(horizon)
+#     for i in range(1, horizon):
+#         inventory_level[i] = inventory_level[i - 1] + inventory_level[i] - future_demand[i]
+#         if reorder_point_arr[i] and t <= len(po_reaching):
+#             vm = po_reaching[t]
+#
+#             optimal_qty = rq[t]
+#             inventory_level[vm] += optimal_qty
+#             optimal_rq[i] = optimal_qty
+#             t += 1
+#
 plt.plot(inventory_level)
 plt.show()
--- model.py
+++ model.py
@@ -1,36 +1,33 @@
-# Copyright (c) Microsoft Corporation.
-# Licensed under the MIT License. 
-
-"""
-This file contains utility functions for builing Dilated CNN model to solve time series forecasting problems.
-"""
-
-
 from math import ceil, log
 from tensorflow.keras.layers import Input, Lambda, Embedding, Conv1D, Dropout, Flatten, Dense, concatenate
 from tensorflow.keras.models import Model
+import keras
 
 
-def create_dcnn_model(
-    seq_len,
-    n_dyn_fea=1,
-    n_outputs=1,
-    n_dilated_layers=3,
-    kernel_size=2,
-    n_filters=3,
-    dropout_rate=0.1,
-    max_cat_id=[100, 100],
+def create_e2e_model(
+        seq_len,
+        n_dyn_fea=1,
+        n_outputs=1,
+        n_dilated_layers=3,
+        kernel_size=2,
+        n_filters=3,
+        dropout_rate=0.1,
+        max_cat_id=[100, 100],
 ):
-    """Create a Dilated CNN model.
+    """Create E2E inventory replenishment model,
+        with a Dilated CNN model for demand forecast.
+        Demand forecast horizon is specified by n_outputs
+        Future VLT is forecasted by 1 step ahead
+        Optimal Reorder Quantity is predicted by 1 step ahead also
 
-    Args: 
+    Args:
         seq_len (int): Input sequence length
         n_dyn_fea (int): Number of dynamic features
         n_outputs (int): Number of outputs of the network
         kernel_size (int): Kernel size of each convolutional layer
         n_filters (int): Number of filters in each convolutional layer
         dropout_rate (float): Dropout rate in the network
-        max_cat_id (list[int]): Each entry in the list represents the maximum value of the ID of a specific categorical variable. 
+        max_cat_id (list[int]): Each entry in the list represents the maximum value of the ID of a specific categorical variable.
 
     Returns:
         object: Keras Model object
@@ -73,9 +70,27 @@ def create_dcnn_model(
     # Concatenate with categorical features
     x = concatenate([conv_out] + cat_flatten)
     x = Dense(16, activation="relu")(x)
-    output = Dense(n_outputs, activation="linear")(x)
+    df_output = Dense(n_outputs, activation="linear")(x)
 
+    # vlt layer
+    vlt_input = Input(shape=(seq_len,))
+    vlt_cat_feature = concatenate([Flatten()(vlt_input)] + cat_flatten)
+    vlt_layer1 = Dense(16, activation="relu")(vlt_cat_feature)
+    vlt_layer2 = Dense(1, activation="relu")(vlt_layer1)
+    vlt_output = vlt_layer2
+
+    # Reorder point (review period)
+    rp_in = Input(shape=(seq_len,))
+    layer3_input = concatenate([x, vlt_layer1, rp_in])
+    layer3_output = Dense(32, activation="relu")(layer3_input)
+
+    # initial_stock
+    initial_stock_in = Input(shape=(seq_len,))
+    layer4_input = concatenate([layer3_output, initial_stock_in])
+    layer4_hidden = Dense(64, activation="relu")(layer4_input)
+    layer4_output = Dense(1, activation="relu")(layer4_hidden)
     # Define model interface
-    model = Model(inputs=[seq_in, cat_fea_in], outputs=output)
+    model = Model(inputs=[seq_in, cat_fea_in, vlt_input, rp_in, initial_stock_in],
+                  outputs=[df_output, layer4_output, vlt_output])
 
     return model
